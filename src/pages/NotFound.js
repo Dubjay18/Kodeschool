455568d859d68dc22a1d779c5ffbbc069ccf90c3
Dubjay18 <jejeniyi7@gmail.com>
2022-05-22 22:36:43 +0100

    fix:error page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Pre from "../images/Preloader_1.gif";
+import { motion } from "framer-motion";
 function NotFound() {
+  const container = {
+    hidden: {
+      opacity: 0,
+      y: "-100vw",
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0.2,
+        duration: 0.3,
+        type: "spring",
+        stiffness: 20,
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: "100vw",
+      transition: {
+        delay: 0.4,
+        duration: 0.5,
+      },
+    },
+  };
   return (
-    <div className="h-screen overflow-hidden flex justify-center items-center bg-slate-50">
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+      className="h-screen overflow-hidden flex justify-center items-center bg-slate-50"
+    >
       <div className="flex flex-col justify-center items-center">
         <img src={Pre} className="w-14" alt="" />
         <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-green-500">
@@ -16,7 +47,7 @@ function NotFound() {
           HomePage
         </Link>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
